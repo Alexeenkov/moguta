9c92b56368d29ad33477e4157ddf3f64edbb87ce
Ilya Alexeenkov <rmfighht@yandex.ru>
2021-11-04 09:13:00 +0300

    Сверстал блоки menu и account, реализовал их функционал

---
+++ js/dropdown.js
@@ -0,0 +1,46 @@
+const burger = document.querySelector('.header__burger');
+const menu = document.querySelector('.menu');
+const arrows = document.querySelectorAll('.menu__arrow');
+const account = document.querySelector('.account__menu');
+const accountBtn = document.querySelector('.account');
+
+burger.addEventListener('click', () => {
+  showDropdownMenu(menu);
+});
+
+accountBtn.addEventListener('click', () => {
+  showDropdownMenu(account);
+});
+
+for (let arrow of arrows) {
+  arrow.addEventListener('click', (e) => {
+    // При нажатии на стрелку находим соседнее с ней выпадающее меню:
+    let dropMenu = e.target.closest('li').querySelector('.menu__dropdown');
+    e.target.classList.toggle('_active');
+    showDropdownMenu(dropMenu);
+  });
+}
+
+// Функция показывает выпадающее меню в зависимости от переданного элемента:
+function showDropdownMenu(elem) {
+  if (elem.classList.contains('_active')) {
+    // Скрываем выпадающее меню
+    elem.classList.remove('_active');
+    elem.style.maxHeight = 0;
+  } else {
+    // Показываем выпадающее меню
+    elem.classList.add('_active');
+    // Пересчитываем высоту выпадающего меню:
+    elem.style.maxHeight = elem.scrollHeight + 'px';
+    if (elem !== menu && elem !== account) {
+      // Выпадающее меню-родитель:
+      let parentMenu = elem.closest('.menu__item').querySelector('.menu__dropdown');
+      // При нажатии на стрелки пересчитываем высоту главного меню:
+      menu.style.maxHeight = menu.scrollHeight + elem.scrollHeight + 'px';
+      if (parentMenu) {
+        // Пересчитываем высоту меню-родителя, если такие есть:
+        parentMenu.style.maxHeight = parentMenu.scrollHeight + elem.scrollHeight + 'px';
+      }
+    }
+  }
+}
